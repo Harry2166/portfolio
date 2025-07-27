63f603f1e3ae085279f48489837659f3a89395a6
Harry2166 <harry2166.hq@gmail.com>
2025-07-27 20:25:57 +0800

    feat: create project card

---
+++ app/components/ProjectCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+import {defineProps} from 'vue';
+
+defineProps({
+	title: {
+		type: String,
+		default: "insert something here"
+	}
+})
+
+</script>
+
+<template>
+  <div class="w-full hover:scale-110" style="background-color: var(--bg-light);"> 
+    <h1>{{title}}</h1>
+  </div>
+</template>
--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <div class="flex flex-row justify-center h-screen">
+    <div class="flex flex-col items-center h-screen">
       <h1 class="text-7xl py-15">My Projects</h1>
+      <div class="flex flex-col items-center">
+        <div class="py-3">
+          <ProjectCard title="Project 0"/>
+        </div>
+        <div class="py-3">
+          <ProjectCard title="Project 1"/>
+        </div>
+      </div>
     </div>
   </div>
 </template>
