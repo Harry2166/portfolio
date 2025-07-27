defc52b241e7e9d621dbc424fb264710645c540c
Harry2166 <harry2166.hq@gmail.com>
2025-07-27 20:01:22 +0800

    feat: add more detail to the front page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -10,9 +10,23 @@
         class="sticky top-0 h-screen flex flex-col items-center justify-center transition-opacity duration-700"
         :class="{ 'opacity-100': activeSlide === index, 'opacity-0': activeSlide !== index }"
       >
-        <h2 v-if="slide.centered" class="text-9xl">{{ slide.title }}</h2>
-        <h2 v-else class="text-5xl font-bold">{{slide.title}}</h2>
-        <p class="mt-2">{{ slide.subtitle }}</p>
+        <h2 v-if="slide.centered" class="text-5xl lg:text-9xl md:text-7xl py-5">{{ slide.title }}</h2>
+        <h2 v-else class="text-5xl py-5">{{slide.title}}</h2>
+        <p class="mt-2 text-2xl">{{ slide.subtitle }}</p>
+        <div v-if="slide.final_slide" class="flex flex-row items-center px-5">
+          <h1 class="px-5">Python</h1>
+          <h1 class="px-5">Typescript</h1>
+          <h1 class="px-5">HTML</h1>
+          <h1 class="px-5">CSS</h1>
+          <h1 class="px-5">Tailwind</h1>
+          <h1 class="px-5">Django</h1>
+          <h1 class="px-5">PostgreSQL</h1>
+          <h1 class="px-5">FastAPI</h1>
+          <h1 class="px-5">Vue</h1>
+          <h1 class="px-5">Svelte</h1>
+          <h1 class="px-5">Godot</h1>
+          <h1 class="px-5">Unity</h1>
+        </div>
       </div>
     </div>
   </div>
@@ -22,8 +36,11 @@
 import { ref, onMounted, onUnmounted, nextTick } from 'vue';
 
 const slides = ref([
-  { title: "Hi I'm Harry.", subtitle: '', centered: true},
-  { title: "I am going to test this slide", subtitle: '', centered: false},
+  { title: "Hi I'm Harry.", subtitle: 'Fullstack Developer, Game Developer, Lifelong Learner', centered: true, final_slide: false },
+  { title: "I am a Computer Science Enthusiast.", subtitle: 'I am interested in all things Computer Science and I am currently pursuing a Computer Science degree in University of the Philippines Diliman.', centered: false, final_slide: false },
+  { title: "I am an aspiring full stack developer, specializing in the backend.", subtitle: 'I can dabble in some frontend though!', centered: false, final_slide: false },
+  { title: "I am also an aspiring game developer, specializing in programming.", subtitle: 'I got interested in Computer Science because of video games!', centered: false, final_slide: false },
+  { title: "Here is my skillset", subtitle: 'I will get the icons for these later', centered: false, final_slide: true },
 ]);
 
 const slideRefs = ref([]);
@@ -31,7 +48,7 @@ const activeSlide = ref(0);
 let observer;
 
 onMounted(async () => {
-  await nextTick(); // Wait for DOM updates
+  await nextTick(); 
 
   observer = new IntersectionObserver(
     (entries) => {
