a4d71060be00198c0df4c6b73e2d59cc08aafbb3
Harry2166 <harry2166.hq@gmail.com>
2025-07-27 20:29:31 +0800

    feat: add logos

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -14,18 +14,15 @@
         <h2 v-else class="text-5xl py-5">{{slide.title}}</h2>
         <p class="mt-2 text-2xl">{{ slide.subtitle }}</p>
         <div v-if="slide.final_slide" class="flex flex-row items-center px-5">
-          <h1 class="px-5">Python</h1>
-          <h1 class="px-5">Typescript</h1>
-          <h1 class="px-5">HTML</h1>
-          <h1 class="px-5">CSS</h1>
-          <h1 class="px-5">Tailwind</h1>
-          <h1 class="px-5">Django</h1>
-          <h1 class="px-5">PostgreSQL</h1>
-          <h1 class="px-5">FastAPI</h1>
-          <h1 class="px-5">Vue</h1>
-          <h1 class="px-5">Svelte</h1>
-          <h1 class="px-5">Godot</h1>
-          <h1 class="px-5">Unity</h1>
+          <Icon name="icon:python" class="px-10" size="3em"/>
+          <Icon name="icon:typescript" class="px-10" size="3em"/>
+          <Icon name="icon:vue" class="px-10" size="3em"/>
+          <Icon name="icon:svelte" class="px-10" size="3em"/>
+          <Icon name="icon:tailwind" class="px-10" size="3em"/>
+          <Icon name="icon:django" class="px-10" size="3em"/>
+          <Icon name="icon:fastapi" class="px-10" size="3em"/>
+          <Icon name="icon:godot" class="px-10" size="3em"/>
+          <Icon name="icon:unity" class="px-10" size="3em"/>
         </div>
       </div>
     </div>
@@ -40,7 +37,7 @@ const slides = ref([
   { title: "I am a Computer Science Enthusiast.", subtitle: 'I am interested in all things Computer Science and I am currently pursuing a Computer Science degree in University of the Philippines Diliman.', centered: false, final_slide: false },
   { title: "I am an aspiring full stack developer, specializing in the backend.", subtitle: 'I can dabble in some frontend though!', centered: false, final_slide: false },
   { title: "I am also an aspiring game developer, specializing in programming.", subtitle: 'I got interested in Computer Science because of video games!', centered: false, final_slide: false },
-  { title: "Here is my skillset", subtitle: 'I will get the icons for these later', centered: false, final_slide: true },
+  { title: "Here is my skillset", subtitle: '', centered: false, final_slide: true },
 ]);
 
 const slideRefs = ref([]);
